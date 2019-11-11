90ae9bd8031da9d48721777e53ac7c0bccebb47c
randy.shahi <randeepshahi@gmail.com>
2019-11-10 18:05:33 -0800

    Can now mock login to blank inbox page

--- MyEmailService/WebClient/js/AccountHandlerService.js
+++ MyEmailService/WebClient/js/AccountHandlerService.js
@@ -6,11 +6,20 @@ function TryLogin(username, password) {
     let adminPassword = "admin";
     if (username == adminUsername
         && password == adminPassword) {
-        // login was successful, return an alert for now
-        alert("login was successful!")
+        // login was successful, send user to their inbox
+        window.location = 'main.html';
     }
     else {
         // login failed, return an alert for now
-        alert("login failed")
+        alert("login failed");
     }
-}
+};
+
+function CreateNewAccount(username, password, confirmPassword){
+    if (password != confirmPassword) {
+        alert("password and confirm password do not match");
+    }
+
+    // Send the information to the node.js server that will handle these request
+    // sending to node.js using a post method
+};
